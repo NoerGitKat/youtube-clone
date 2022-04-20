801ae0605d21f0126b1e3e8c6cc6e86085d23148
Noer Paanakker <noer@lightworksweb.nl>
2022-04-20 17:09:50 +0200

    finished user model

--- server/src/modules/user/user.model.ts
+++ server/src/modules/user/user.model.ts
@@ -1,6 +1,31 @@
 import { getModelForClass, prop, pre } from "@typegoose/typegoose";
+import argon2 from "argon2";
 
+@pre<User>("save", async function (next) {
+  if (this.isModified("password") || this.isNew) {
+    const hashedPassword = await argon2.hash(this.password);
+    this.password = hashedPassword;
+
+    return next();
+  }
+})
 export class User {
   @prop({ required: true, unique: true })
   public username: string;
+
+  @prop({ required: true, unique: true })
+  public email: string;
+
+  @prop({ required: true })
+  public password: string;
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return await argon2.verify(this.password, password);
+  }
 }
+
+export const UserModel = getModelForClass(User, {
+  schemaOptions: {
+    timestamps: true,
+  },
+});
